9f708ccd665641cb022a6dbc0c92204d9252e22a
genautkin <genautkin@gmail.com>
2022-07-07 18:08:33 +0300

    modules

---
+++ 7.7.2022/modules2.js
@@ -0,0 +1,11 @@
+module.exports = {
+    print: function(message) {
+        console.log(message);
+    },
+    print2: function(message) { 
+        console.log(message+'2'); },
+    print3: function(message) {
+        console.log(message+'3');
+    }
+}
+

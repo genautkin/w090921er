78dae47f7cc100145748385f47def0a03240a9c1
genautkin <genautkin@gmail.com>
2022-07-18 20:14:26 +0300

    form git | post

--- 18.07.2022/bootstrap/app.js
+++ 18.07.2022/bootstrap/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const PORT = process.env.PORT || 3000;
 const app = express();
-const images = require('./modules/images')
+const images = require('./modules/images');
+const bodyParser = require('body-parser');
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
 const path = require("path");
@@ -15,12 +18,25 @@ const viewsPath = path.join(__dirname,'/templates/views')
 app.set('views', viewsPath)
 
 app.get("" , (req, res) => {
-
-    res.render("index",{imagesArray: images});
+    let _images = images
+    if (!!req.query.size) {
+        _images=  _images.slice(0, req.query.size)
+    }
+    res.render("index",{imagesArray: _images});
 });
 
-app.get('*', (req, res) => { 
-    res.redirect(303,"/") })
+app.get("/form" , (req, res) => {
+    console.log(req.query)
+    res.render("form");
+});
+
+app.post("/form" , (req, res) => {
+    console.log(req.body.name);
+    res.render("form");
+});
+
+// app.get('*', (req, res) => { 
+//     res.redirect(303,"/") })
 
 
 

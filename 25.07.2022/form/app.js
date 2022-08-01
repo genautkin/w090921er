7db15c213a7caadc1d482465a91212846dffc79f
genautkin <genautkin@gmail.com>
2022-08-01 21:19:24 +0300

    create user

--- 25.07.2022/form/app.js
+++ 25.07.2022/form/app.js
@@ -6,7 +6,7 @@ const Book = require('./modules/mongoDb');
 const bodyParser = require('body-parser');
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
 
 
 const path = require("path");
---
+++ restApi/databases/mongoDb.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+ 
+mongoose.connect('mongodb://localhost/books')
+.then( () => console.log('connecting to mongodb!') )
+.catch( err => console.error('Could not connect to mongodb', err) );
+
+exports.modules = mongoose;

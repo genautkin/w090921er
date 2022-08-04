1e5afba884012e419b480a403552539a9fe8e80f
genautkin <genautkin@gmail.com>
2022-08-04 17:54:22 +0300

    remove not in use mongo

--- restApi/app.js
+++ restApi/app.js
@@ -3,7 +3,6 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 const user = require("./routes/user");
-const mongoose = require("./databases/mongoDb");
 
 
 app.use('/user',user);

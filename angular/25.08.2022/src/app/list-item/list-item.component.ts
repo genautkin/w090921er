baf04581d53ef7aae953b1cc973e41b0b93a223c
genautkin <genautkin@gmail.com>
2022-08-25 20:31:14 +0300

    todo list

--- angular/25.08.2022/src/app/list-item/list-item.component.ts
+++ angular/25.08.2022/src/app/list-item/list-item.component.ts
@@ -8,7 +8,8 @@ import { Component, Input, OnInit } from '@angular/core';
 export class ListItemComponent implements OnInit {
 
   constructor() { }
-  @Input() item="test";
+  @Input() item:string = "";
+  @Input() index:string = "";
   ngOnInit(): void {
   }
 
--- angular/25.08.2022/src/app/todolist/todolist.component.ts
+++ angular/25.08.2022/src/app/todolist/todolist.component.ts
@@ -15,7 +15,19 @@ export class TodolistComponent implements OnInit {
   menuItems= ["Home", "About", "Contact", "Contact Info"]
 
   ngOnInit(): void {
-    
+    // setInterval(() => {
+    //   this.menuItems.push(this.makeid(5))
+    // }, 2000);
   }
+  makeid(length:number):string {
+      var result           = '';
+      var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      var charactersLength = characters.length;
+      for ( var i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * 
+  charactersLength));
+    }
+    return result;
+}
 
 }
